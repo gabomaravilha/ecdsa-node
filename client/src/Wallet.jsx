e044b65c0c46638ee117dcc9b7fc09bf62e9817c
gabomaravilha <gabrieldru@gmail.com>
2023-06-30 20:57:30 -0400

    generating keys and using in wallet component

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,15 @@
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import {toHex, utf8ToBytes} from "ethereum-cryptography/utils";
+import { sha256 } from "ethereum-cryptography/sha256.js";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value
+    setPrivateKey(privateKey);
+    console.log(privateKey)
+    const address = secp256k1.getPublicKey(sha256(utf8ToBytes(privateKey)))
+    setAddress(address)
     if (address) {
       const {
         data: { balance },
