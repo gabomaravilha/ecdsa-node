fab9d8616223f610c65b228cdf05a517e05cc440
gabomaravilha <gabrieldru@gmail.com>
2023-07-01 22:31:49 -0400

    fixing wallet values

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -7,8 +7,8 @@ function Wallet({ address, setAddress, balance, setBalance, privateKey, setPriva
   async function onChange(evt) {
     const privateKey = evt.target.value
     setPrivateKey(privateKey);
-    console.log(privateKey)
-    const address = secp256k1.getPublicKey(sha256(utf8ToBytes(privateKey)))
+    // const address = secp256k1.getPublicKey(sha256(utf8ToBytes(privateKey)))
+    const address = toHex(secp256k1.getPublicKey(privateKey));
     setAddress(address)
     if (address) {
       const {
@@ -26,8 +26,12 @@ function Wallet({ address, setAddress, balance, setBalance, privateKey, setPriva
 
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input placeholder="Type an address, for example: 0x1" value={privateKey} onChange={onChange}></input>
       </label>
+      <div>
+        Address: {address}
+      </div>
+
 
       <div className="balance">Balance: {balance}</div>
     </div>
